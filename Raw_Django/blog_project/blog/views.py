202b1530287d1edf391994066552351f827eabec
DHughes666 <graziemolto6@gmail.com>
2024-02-09 23:26:28 +0100

    Raw_Django _Blog project ...Blog app _model and view features in progress ... so far so interesting...

--- Raw_Django/blog_project/blog/views.py
+++ Raw_Django/blog_project/blog/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from django.http import Http404
 from .models import Post
 
 
 # Create your views here.
 def post_list(request):
     posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    context = {'posts': posts}
+    return render(request, 'blog/post/list.html', context=context)
+
+def post_detail(request, id):
+    try:
+        post = Post.published.get(id=id)
+    except Post.DoesNotExist:
+        raise Http404("No Post found.")
+    context = {'post': post}
+    return render(request, 'blog/post/detail.html', context=context)
