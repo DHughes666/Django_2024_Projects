1e52cf2409f2ece7048eede0fdf183f36d2d5781
DHughes666 <graziemolto6@gmail.com>
2024-02-10 00:03:14 +0100

    Raw_Django _Blog project ...View ...Retrieving data with 'get_object_or_404'

--- Raw_Django/blog_project/blog/views.py
+++ Raw_Django/blog_project/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Post
 
@@ -10,6 +10,7 @@ def post_list(request):
     return render(request, 'blog/post/list.html', context=context)
 
 def post_detail(request, id):
-    post = get_list_or_404(Post, id=id, Status=Post.Status.PUBLISHED)
+    #Using get_object_or_404 is neater than using the try/except method
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
     context = {'post': post}
     return render(request, 'blog/post/detail.html', context=context)
