6176cccee2ac8fc81768d8f5ec8b92e3d7253b2a
DHughes666 <graziemolto6@gmail.com>
2024-02-21 00:45:24 +0100

    Filter, Search... Completed

--- Raw_Django/blog_project/blog/views.py
+++ Raw_Django/blog_project/blog/views.py
@@ -8,7 +8,8 @@ from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import (
+    SearchVector, SearchQuery, SearchRank)
 
 
 
@@ -120,8 +121,12 @@ def post_search(request):
         form  = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title', weight='A') + \
+                            SearchVector('body', weight='B')
+            search_query = SearchQuery(query)
             results = Post.published.annotate(
-                search = SearchVector('title', 'body'),
-            ).filter(search=query)
+                search = search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(rank__gte=0.3).order_by('-rank')
     context = {'form': form, 'query': query, 'results': results}
     return render(request, 'blog/post/search.html', context)
