abd6eb9de6e0caedf194518ca163687252f58706
DHughes666 <graziemolto6@gmail.com>
2024-02-22 03:40:26 +0100

    Project ...Paused

--- Raw_Django/socialMedia_project/account/views.py
+++ Raw_Django/socialMedia_project/account/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
 from .forms import LoginForm
 
@@ -24,5 +25,10 @@ def user_login(request):
     else:
         form = LoginForm()
     context = {'form': form}
-    return render(request, 'accounts/login.html', context)
+    return render(request, 'account/login.html', context)
+
+@login_required
+def dashboard(request):
+    context = {'section': 'dashboard'}
+    return render(request, 'account/dashboard.html', context)
         
