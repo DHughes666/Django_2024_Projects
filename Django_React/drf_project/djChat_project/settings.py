e67fa292b7a1d0a167bb3170ed5667bf210438a0
DHughes666 <graziemolto6@gmail.com>
2024-02-26 01:36:28 +0100

    Django_channels_react project ...Setting up environmental varialbes

--- Django_React/drf_project/djChat_project/settings.py
+++ Django_React/drf_project/djChat_project/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6egt5a-nf32d-w@1w#5f0k5p788@j-81i+m^y)4af2x8um9k09'
-
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = []
 
