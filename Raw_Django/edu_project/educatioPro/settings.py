02cab755bcf5d26a659e29228bb92e5493569903
DHughes666 <graziemolto6@gmail.com>
2024-04-06 22:58:24 +0100

    Chat app... Initiated

--- Raw_Django/edu_project/educatioPro/settings.py
+++ Raw_Django/edu_project/educatioPro/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'accounty.apps.AccountyConfig',
     'courses.apps.CoursesConfig',
     'students.apps.StudentsConfig',
+    'chat',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
