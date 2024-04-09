4499906e93819259e077461c369ce9b63e8b711d
DHughes666 <graziemolto6@gmail.com>
2024-04-09 01:24:14 +0100

    Sending messages via channels

--- Raw_Django/edu_project/educatioPro/settings.py
+++ Raw_Django/edu_project/educatioPro/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'daphne',
+    'channels',
     'accounty.apps.AccountyConfig',
     'courses.apps.CoursesConfig',
     'students.apps.StudentsConfig',
